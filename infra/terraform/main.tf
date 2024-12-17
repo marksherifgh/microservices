51c4c629fe7244bd05966f203717a101ac4a7864
Marc Sherif <msherif@synapse-analytics.io>
2024-12-17 18:29:53 +0200

    chore: switch from Azure to AWS

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,34 +1,67 @@
-provider "azurerm" {
-  features {}
+provider "aws" {
+  region = "eu-west-1"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "microservice-rg"
-  location = "UAE North"
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "microservice-aks"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "microservice-aks"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_B1ms"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    Environment = "dev"
-  }
+resource "aws_subnet" "subnet_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = true
 }
 
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config[0].value
-  sensitive = true
+resource "aws_security_group" "eks_sg" {
+  name        = "eks_sg"
+  description = "EKS Security Group"
+  vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = "microservice-eks"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  vpc_config {
+    subnet_ids = [aws_subnet.subnet_a.id]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_policy_attachment]
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks.name
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.eks.endpoint
+}
+
+output "eks_cluster_arn" {
+  value = aws_eks_cluster.eks.arn
+}
+
+output "eks_cluster_role_arn" {
+  value = aws_iam_role.eks_cluster_role.arn
 }
