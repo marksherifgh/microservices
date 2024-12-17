ed40b4704825102c87020b3650218b858f21ea01
Marc Sherif <msherif@synapse-analytics.io>
2024-12-17 19:12:40 +0200

    chore: change eks-role name

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -44,7 +44,7 @@ resource "aws_eks_cluster" "eks" {
 }
 
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
+  name = "eks-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
