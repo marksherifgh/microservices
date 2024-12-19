89655f60824e534793f8fd4af07855bf807a9f01
Marc Sherif <msherif@synapse-analytics.io>
2024-12-19 03:54:37 +0200

    chore: use tf modules instead of vanilla terraform

--- infra/terraform/vpc.tf
+++ infra/terraform/vpc.tf
@@ -1,10 +1,29 @@
-resource "aws_vpc" "vpc" {
-  cidr_block           = var.vpc_cidr_block
-  enable_dns_support   = true
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = var.vpc_tag_name
+  cidr = var.vpc_cidr_block
+
+  azs             = var.az
+  private_subnets = [var.private_subnet_cidr_block_1, var.private_subnet_cidr_block_2]
+  public_subnets  = [var.public_subnet_cidr_block_1, var.public_subnet_cidr_block_2]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
 
-  tags = {
-    Name                                            = var.vpc_tag_name
+  public_subnet_tags = {
     "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                        = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"               = 1
+  }
+  tags = {
+    "Name" = var.vpc_tag_name
   }
 }
+
